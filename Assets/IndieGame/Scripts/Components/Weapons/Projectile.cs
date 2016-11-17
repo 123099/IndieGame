7911e63aaf0eb4d486e9056aa54efe616153a75d
123099 <khazovalex97@gmail.com>
2016-11-17 13:00:30 +0100

    Added object pool
    Started working on projectiles

---
+++ Assets/IndieGame/Scripts/Components/Weapons/Projectile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour
+{
+    [Tooltip("The range thr projectile can fly to. After this range, the projectile is destroyed.")]
+
+}
