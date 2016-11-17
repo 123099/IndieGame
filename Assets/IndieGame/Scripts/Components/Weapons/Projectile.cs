35e9c56b62dab39e568bc0135970ee8665be1f05
123099 <khazovalex97@gmail.com>
2016-11-17 14:15:09 +0100

    Started working on projectiles

--- Assets/IndieGame/Scripts/Components/Weapons/Projectile.cs
+++ Assets/IndieGame/Scripts/Components/Weapons/Projectile.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public abstract class Projectile : MonoBehaviour
 {
-    //[Tooltip("The range thr projectile can fly to. After this range, the projectile is destroyed.")]
+    [Tooltip("The range the projectile can fly to. After this range, the projectile is destroyed.")]
+    [SerializeField] protected float range;
+
+    [Tooltip("The speed with which to shoot the projectile")]
+    [SerializeField] protected float speed;
+
+    [Tooltip("Whether the projectile deals AoE damage or not")]
+    [SerializeField] protected bool doesAoEDamage;
+
+    [Tooltip("The radius of the AoE damage. Ignored if AoE is turned off")]
+    [SerializeField] protected float AoERadius;
+
+    [Tooltip("The damage the projectile deals")]
+    [SerializeField] protected float damage;
+
 
 }
