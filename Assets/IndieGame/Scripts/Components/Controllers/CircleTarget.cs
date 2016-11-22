ce0d36c669a8f5aa4bdb767bed1c943362fa8bcd
123099 <khazovalex97@gmail.com>
2016-11-22 12:33:34 +0100

    Added water and fire abilities

---
+++ Assets/IndieGame/Scripts/Components/Controllers/CircleTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates the object around a target.
+/// This can be used with or without a rigidbody.
+/// </summary>
+public class CircleTarget : MonoBehaviour
+{
+    [Tooltip("The target around which to rotate")]
+    [SerializeField] protected Transform target;
+
+    [Tooltip("The speed with which to rotate around the target")]
+    [SerializeField] protected float speed;
+
+    protected Rigidbody cachcedRigidbody;
+
+	protected virtual void Awake ()
+    {
+        cachcedRigidbody = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void Update ()
+    {
+        //Select method of rotation
+        if(cachcedRigidbody != null)
+        {
+            cachcedRigidbody.RotateAround(target.position, target.up, speed);
+        }
+        else
+        {
+            transform.RotateAround(target.position, target.up, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/IndieGame/Scripts/Utils/RigidbodyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RigidbodyExtensions {
+    
+    /// <summary>
+    /// Rotates the rigidbody around a specific point and an axis by the provided angle
+    /// </summary>
+    public static void RotateAround(this Rigidbody rigidbody, Vector3 point, Vector3 axis, float angularSpeed)
+    {
+        angularSpeed *= Time.deltaTime;
+
+        //Create the rotation around the provided axis
+        Quaternion rotation = Quaternion.AngleAxis(angularSpeed, axis);
+
+        //Translate the point to the origin, rotate around it, and translate back
+        rigidbody.MovePosition(rotation * ( rigidbody.transform.position - point ) + point);
+
+        //Rotate the rigidbody
+        rigidbody.MoveRotation(rotation * rigidbody.transform.rotation);
+    }
+}
