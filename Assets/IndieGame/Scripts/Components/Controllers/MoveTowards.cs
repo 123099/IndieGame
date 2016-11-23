54bf64df0ee72f21412a0cb932ad1ea4821db143
123099 <khazovalex97@gmail.com>
2016-11-23 09:07:01 +0100

    Completed the fire boss ability and fixed the move towards script

--- Assets/IndieGame/Scripts/Components/Controllers/MoveTowards.cs
+++ Assets/IndieGame/Scripts/Components/Controllers/MoveTowards.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// Defines the action a script would take when finishing a certain task on an object.
+/// </summary>
+public enum GoalAction
+{
+    Stop,
+    SelfDestruct,
+    DeactivateSelf,
+    DeactivateSelfObject
+}
+
+/// <summary>
 /// Moves an object towards another object at a specified speed.
 /// Can be used with or without a rigid body.
 /// </summary>
@@ -23,21 +34,15 @@ public class MoveTowards : MonoBehaviour
     [Tooltip("What to do when the object reaches the target")]
     [SerializeField] protected GoalAction goalAction;
 
+    protected Vector3 startLocation;
     protected bool isStopped;
-
-    protected enum GoalAction
-    {
-        Stop,
-        SelfDestruct,
-        DeactivateSelf,
-        DeactivateSelfObject
-    }
 
     protected Rigidbody cachedRigidbody;
 
     protected virtual void Awake ()
     {
         cachedRigidbody = GetComponent<Rigidbody>();
+        startLocation = transform.position;
     }
 
     protected virtual void OnEnable ()
@@ -60,22 +65,7 @@ public class MoveTowards : MonoBehaviour
             //Check if we reached the target
             if (Vector3.Distance(transform.position, target.position) <= thresholdDistance)
             {
-                //Decide what to do
-                switch (goalAction)
-                {
-                    case GoalAction.Stop:
-                        isStopped = true;
-                        break;
-                    case GoalAction.SelfDestruct:
-                        Destroy(gameObject);
-                        break;
-                    case GoalAction.DeactivateSelf:
-                        enabled = false;
-                        break;
-                    case GoalAction.DeactivateSelfObject:
-                        gameObject.SetActive(false);
-                        break;
-                }
+                EndMovement();
 
                 //Stop moving this iteration
                 return;
@@ -83,20 +73,85 @@ public class MoveTowards : MonoBehaviour
         }
         else
         {
+            //Check our distance from the start
+            if(Vector3.Distance(startLocation, transform.position) >= movementDistance)
+            {
+                EndMovement();
 
+                //Stop moving this iteration
+                return;
+            }
         }
 
-        //Get direction vector between us and the target
-        Vector3 direction = ( target.position - transform.position ).normalized;
+        //Get direction vector
+        Vector3 direction;
+
+        //If the target exists, get direction from us to the target
+        if (target != null)
+        {
+            direction = ( target.position - transform.position ).normalized;
+        }
+        else
+        {
+            //Look forward
+            direction = transform.forward;
+        }
+
+        //Look towards the direction vector
+        transform.rotation = Quaternion.LookRotation(direction, transform.up);
 
         //Select our method of transportation
         if(cachedRigidbody != null)
         {
-            cachedRigidbody.velocity = direction * speed;
+            cachedRigidbody.velocity = transform.forward * speed;
         }
         else
         {
-            transform.Translate(direction * speed * Time.deltaTime);
+            transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
         }
     }
+
+    protected virtual void EndMovement ()
+    {
+        //Decide what to do
+        switch (goalAction)
+        {
+            case GoalAction.Stop:
+                isStopped = true;
+                break;
+            case GoalAction.SelfDestruct:
+                Destroy(gameObject);
+                break;
+            case GoalAction.DeactivateSelf:
+                enabled = false;
+                break;
+            case GoalAction.DeactivateSelfObject:
+                gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    #region Public members
+
+    public virtual void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    public virtual void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public virtual void SetThresholdDistance(float thresholdDistance)
+    {
+        this.thresholdDistance = thresholdDistance;
+    }
+
+    public virtual void SetGoalAction(GoalAction goalAction)
+    {
+        this.goalAction = goalAction;
+    }
+
+    #endregion
 }
---
+++ Assets/IndieGame/Scripts/Components/Entity/Attacks/LavaAttack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Attacks",
+             "Lava Pools",
+             "Spawns lava pools at a certain rate that move towards the attack executer and damage everything in their way")]
+public class LavaAttack : Attack
+{
+    [Tooltip("The lava pool prefab to spawn for the attack")]
+    [SerializeField] protected Damager lavaPoolPrefab;
+
+    [Tooltip("The amount of lava pools to spawn around the attack executer")]
+    [Range(0, 10)] [SerializeField] protected int lavaPoolCountPerSpawn;
+
+    [Tooltip("The movement speed of the lava pools towards the attack executer")]
+    [SerializeField] protected float lavaPoolSpeed;
+
+    protected override IEnumerator DoLaunchAttack (Action onAttackComplete)
+    {
+        //The spread angle between pools should be the golden angle for maximum spread
+        const float angleBetweenPools = 137.508f;
+
+        //Cache the transform of the executer for easier access
+        Transform attackExecuterTransform = attackExecuter.Value.transform;
+
+        for(int i = 0; i < lavaPoolCountPerSpawn; ++i)
+        {
+            //Spawn a lava pool
+            Damager lavaPool = Instantiate(lavaPoolPrefab);
+
+            //Calculate rotated direction by the angle between the pools away from the executer
+            Vector3 vectorToPool = Quaternion.AngleAxis(angleBetweenPools * i, attackExecuterTransform.up) * attackExecuterTransform.right;
+
+            //Position the pool at the new position with range units away from the executer
+            lavaPool.transform.position = attackExecuterTransform.position + vectorToPool * range;
+
+            //Add the executer to the exclusion list of the lava pool
+            lavaPool.ExcludeTarget(attackExecuterTransform);
+
+            //Add a move towards component to the lava 
+            MoveTowards moveTowardsComponent = lavaPool.gameObject.AddComponent<MoveTowards>();
+
+            //Set the target of the move towards component to be the attack executer
+            moveTowardsComponent.SetTarget(attackExecuterTransform);
+
+            //Set the speed of the lava pools
+            moveTowardsComponent.SetSpeed(lavaPoolSpeed);
+
+            //Set a small threshold distance to prevent floating point problems
+            moveTowardsComponent.SetThresholdDistance(0.1f);
+
+            //Set the goal action of the move towards to destroy the lava pool
+            //TODO: probably change the goal action to do nothing, and then play some animation, that then destroys the object
+            moveTowardsComponent.SetGoalAction(GoalAction.SelfDestruct);
+
+            //Wait for a few frames to improve performance and give a nice illusion of spawning in series
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        //We are done with the attack
+        if(onAttackComplete != null)
+        {
+            onAttackComplete.Invoke();
+        }
+    }
+}
