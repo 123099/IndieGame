fdf4dbb73ff23a03cbb85c91524cdf9d44e88407
123099 <khazovalex97@gmail.com>
2016-11-25 16:01:44 +0100

    Fixed BGM music, added music menu to Unity

---
+++ Assets/IndieGame/Editor/MusicMenuItems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class MusicMenuItems
+{ 
+    [MenuItem("IndieGame/Create/Music/BGM/Village")]
+    public static void CreateVillageBGM ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("BGM_Village", false, false);
+
+        if(prefab != null)
+        {
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("BGM_Village");
+        }
+        else
+        {
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Village");
+        }
+    }
+}
