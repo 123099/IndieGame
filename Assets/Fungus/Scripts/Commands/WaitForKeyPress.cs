3dbbea80b04fc777e5ca8fd750889d059a791129
123099 <khazovalex97@gmail.com>
2016-11-27 22:44:27 +0100

    Created Start scene for the game

---
+++ Assets/Fungus/Scripts/Commands/WaitForKeyPress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Flow",
+             "Wait For Key Press",
+             "Waits until a specific key is pressed and then continues execution")]
+public class WaitForKeyPress : Command
+{
+    [Tooltip("The key to wait for to be pressed")]
+    [SerializeField] protected KeyCode keyToPress;
+
+    [Tooltip("Should we wait for any key press?")]
+    [SerializeField] protected BooleanData waitForAnyKey;
+
+    protected virtual IEnumerator DoWaitKeypress ()
+    {
+        while(true)
+        {
+            if(( waitForAnyKey.Value == true && Input.anyKey) || Input.GetKeyDown(keyToPress))
+            {
+                Continue();
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+    #region Public members
+
+    public override void OnEnter ()
+    {
+        StartCoroutine(DoWaitKeypress());
+    }
+
+    #endregion
+}
