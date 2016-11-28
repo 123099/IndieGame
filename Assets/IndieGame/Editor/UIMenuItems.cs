daf0c24c9314378345c68db663bdac8b43dc4f21
123099 <khazovalex97@gmail.com>
2016-11-28 20:41:10 +0100

    Added options menu

---
+++ Assets/IndieGame/Editor/UIMenuItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class UIMenuItems
+{
+    [MenuItem("IndieGame/Create/UI/Options Menu")]
+    public static void CreateOptionsMenu ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("OptionsMenu", false, false);
+
+        if (prefab == null)
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("OptionsMenu");
+        else
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("OptionsMenu");
+    }
+}
