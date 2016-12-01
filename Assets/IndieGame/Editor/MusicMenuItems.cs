e2c23d42832afae19248553132c8aa3b4ee0d029
123099 <khazovalex97@gmail.com>
2016-12-01 10:45:29 +0100

    added music menu items

--- Assets/IndieGame/Editor/MusicMenuItems.cs
+++ Assets/IndieGame/Editor/MusicMenuItems.cs
@@ -34,4 +34,64 @@ public static class MusicMenuItems
             EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Fire");
         }
     }
+
+    [MenuItem("IndieGame/Create/Music/BGM/Water")]
+    public static void CreateWaterBGM ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("BGM_Water", false, false);
+
+        if (prefab != null)
+        {
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("BGM_Water");
+        }
+        else
+        {
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Water");
+        }
+    }
+
+    [MenuItem("IndieGame/Create/Music/BGM/Earth")]
+    public static void CreateEarthBGM ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("BGM_Earth", false, false);
+
+        if (prefab != null)
+        {
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("BGM_Earth");
+        }
+        else
+        {
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Earth");
+        }
+    }
+
+    [MenuItem("IndieGame/Create/Music/BGM/Wind")]
+    public static void CreateWindBGM ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("BGM_Wind", false, false);
+
+        if (prefab != null)
+        {
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("BGM_Wind");
+        }
+        else
+        {
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Wind");
+        }
+    }
+
+    [MenuItem("IndieGame/Create/Music/BGM/Boss")]
+    public static void CreateBossBGM ()
+    {
+        GameObject prefab = EditorUtils.SpawnPrefab("BGM_Boss", false, false);
+
+        if (prefab != null)
+        {
+            EditorUtils.ShowPrefabCreationSucceededMessageInSceneWindow("BGM_Boss");
+        }
+        else
+        {
+            EditorUtils.ShowPrefabCreationFailedMessageInSceneWindow("BGM_Boss");
+        }
+    }
 }
